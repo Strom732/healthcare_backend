53eb91f09fdc8a1e0e34acb119b07b8ed320b22b
Strom732 <abhinandansingh9325@gmail.com>
2025-07-11 12:49:45 +0530

    login issuss

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
 
     path('patients/', views.patient_list, name='patient_list'),
